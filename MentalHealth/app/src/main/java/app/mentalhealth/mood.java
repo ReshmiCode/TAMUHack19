782a895b8d374c09d877e3267222acd893e10b85
aolinyang <allenyang2813@gmail.com>
2019-01-26 23:30:46 -0600

    Added some back-end to moods, can now update them with buttons

--- MentalHealth/app/src/main/java/app/mentalhealth/mood.java
+++ MentalHealth/app/src/main/java/app/mentalhealth/mood.java
@@ -2,8 +2,24 @@ package app.mentalhealth;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class mood extends Activity {
 
@@ -12,11 +28,61 @@ public class mood extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
 
+        FirebaseApp.initializeApp(this);
+        // Access a Cloud Firestore instance from your Activity
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi");
+        final Map<String, Object> data = new HashMap<>();
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        final Map<String, Object> fdata = document.getData();
+                        data.put("1 day ago",fdata.get("1 day ago"));
+                        data.put("2 days ago",fdata.get("2 days ago"));
+                        data.put("3 days ago",fdata.get("3 days ago"));
+                        data.put("4 days ago",fdata.get("4 days ago"));
+                        data.put("5 days ago",fdata.get("5 days ago"));
+                        data.put("6 days ago",fdata.get("6 days ago"));
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+        //HAPPY
         final ImageButton happyButton = findViewById(R.id.happiness);
         happyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
 
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "happiness");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
             }
         });
 
@@ -24,6 +90,27 @@ public class mood extends Activity {
         sadButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "sadness");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
 
             }
         });
@@ -32,14 +119,56 @@ public class mood extends Activity {
         angryButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "anger");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
 
             }
         });
 
-        final ImageButton tiredButton = findViewById(R.id.surprise);
-        tiredButton.setOnClickListener(new View.OnClickListener() {
+        final ImageButton surpriseButton = findViewById(R.id.surprise);
+        surpriseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "surprise");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
 
             }
         });
@@ -48,14 +177,56 @@ public class mood extends Activity {
         neutralButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "neutral");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
 
             }
         });
 
-        final ImageButton depressedButton = findViewById(R.id.fear);
-        depressedButton.setOnClickListener(new View.OnClickListener() {
+        final ImageButton fearButton = findViewById(R.id.fear);
+        fearButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Map<String, Object> fdata = data;
+                fdata.put("6 days ago", fdata.get("5 days ago"));
+                fdata.put("5 days ago", fdata.get("4 days ago"));
+                fdata.put("4 days ago", fdata.get("3 days ago"));
+                fdata.put("3 days ago", fdata.get("2 days ago"));
+                fdata.put("2 days ago", fdata.get("1 day ago"));
+                fdata.put("1 day ago", "fear");
+                db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi")
+                        .set(fdata)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
 
             }
         });
--- MentalHealth/app/src/main/java/app/mentalhealth/moodhistory.java
+++ MentalHealth/app/src/main/java/app/mentalhealth/moodhistory.java
@@ -1,13 +1,57 @@
 package app.mentalhealth;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class moodhistory extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+import android.content.Context;
+
+public class moodhistory extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moodhistory);
+
+        FirebaseApp.initializeApp(this);
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
     }
 }
