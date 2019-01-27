adbfa3e5372d8839a4cad9af9e80acb3056d441a
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-01-27 09:26:51 -0600

    Doctor page updated and fingerprint log in added

--- MentalHealth/app/src/main/java/app/mentalhealth/DoctorActivity.java
+++ MentalHealth/app/src/main/java/app/mentalhealth/DoctorActivity.java
@@ -1,16 +1,21 @@
 package app.mentalhealth;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -42,8 +47,8 @@ public class DoctorActivity extends Activity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         final Map<String, Object> fdata = document.getData();
-                        String ID = fdata.get("patientID").toString();  // doctors have their patient's IDs
-                        // CHANGE TO ID
+                        String ID = fdata.get("patientID1").toString();  // doctors have their patient's IDs
+                        // CHANGE ID
                         DocumentReference patientRef = db.collection("pastMoods").document("6cNOF7Dbfo59FB968Iqi");
                         getPatient(patientRef, ID);
                     } else {
@@ -52,6 +57,25 @@ public class DoctorActivity extends Activity {
                 } else {
                     Log.d(TAG, "get failed with ", task.getException());
                 }
+            }
+        });
+
+        final Button add = findViewById(R.id.addPatient);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // startActivity(new Intent(DoctorActivity.this, AddPatient.class));
+                // ADD AFTER ADD PATIENT CLASS
+            }
+        });
+
+        final Button logOut = findViewById(R.id.logOut);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signOut();
+                startActivity(new Intent(DoctorActivity.this, LoginActivity.class));
+                // startActivity(new Intent(DoctorActivity.this, AddPatient.class));
+                // ADD AFTER ADD PATIENT CLASS
             }
         });
     }
@@ -75,4 +99,17 @@ public class DoctorActivity extends Activity {
             }
         });
     }
+
+    FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user == null) {
+                // user auth state is changed - user is null
+                // launch login activity
+                startActivity(new Intent(DoctorActivity.this, LoginActivity.class));
+                finish();
+            }
+        }
+    };
 }
