d67828a628c84c47e3523ba65edcecdad0e57533
aolinyang <allenyang2813@gmail.com>
2019-01-26 20:09:55 -0600

    Added the page for picking moods

--- MentalHealth/app/src/main/java/app/mentalhealth/LoginActivity.java
+++ MentalHealth/app/src/main/java/app/mentalhealth/LoginActivity.java
@@ -1,6 +1,7 @@
 package app.mentalhealth;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends Activity {
@@ -9,5 +10,8 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(this, mood.class);
+        startActivity(intent);
     }
 }
---
+++ MentalHealth/app/src/main/java/app/mentalhealth/moodhistory.java
@@ -0,0 +1,13 @@
+package app.mentalhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class moodhistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moodhistory);
+    }
+}
